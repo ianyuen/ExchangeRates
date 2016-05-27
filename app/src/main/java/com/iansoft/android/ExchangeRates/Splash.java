5aa9196aa993426268e40b6beb2160a490b7e07b
ianyuen <thanhianyuen@gmail.com>
2016-05-28 01:00:15 +0700

    - wrong name

--- app/src/main/java/com/iansoft/android/ExchangeRates/Splash.java
+++ app/src/main/java/com/iansoft/android/ExchangeRates/Splash.java
@@ -7,6 +7,6 @@ public class Splash extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.splash);
 	}
 }
